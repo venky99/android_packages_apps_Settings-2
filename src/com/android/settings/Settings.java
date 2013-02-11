662c856f4a7bd923bd9ff0a855b2c399488b862e
itsmikeramsay <ronaldramsayii@gmail.com>
2013-02-10 22:48:59 -0500

    Fixed derp Sorry guys, gotta get this repo thing straightened out

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -123,7 +123,7 @@ public class Settings extends PreferenceActivity
             R.id.date_time_settings,
             R.id.about_settings,
             R.id.accessibility_settings,
-            R.id.interface_section
+            R.id.interface_section,
             R.id.goo_manager
     };
 
