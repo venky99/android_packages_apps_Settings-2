SlimRoms/packages_apps_Settings

Settings: Lockscreen custom shortcuts and 8 targets (2/2)
Normal phones need a bit more love on Lockscreen.

This commit adds

- the ability to add additional shortcuts on lockscreen. (so you can use 5 targets + 6 shortcuts)
- Ability to open shortcut only with longpress
- Ability to use 8 targets on all phones (8 targets / shortcuts disabled)
- fix cutoff lockscreenring
- some minor fixes
- only for phones at the moment. Tablets have already 8 targets

Patchsets:
- cleanup
- fix reset targets on mode change
- fix string
- added the other png's from jeff
- rebase

cheers

Change-Id: I8a9da05a20f07badd48b521b9610902a07458010

src/com/android/settings/slim/ApplicationsDialogPreference.java
@@ -1,0 +1,166 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ * This code has been modified. Portions copyright (C) 2012, ParanoidAndroid Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ApplicationsDialogPreference extends SettingsPreferenceFragment {
+
+    public PackageManager mPackageManager;
+    public List<ResolveInfo> mInstalledApps;
+    public AppAdapter mAppAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public class AppItem implements Comparable<AppItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(AppItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    public class AppAdapter extends BaseAdapter {
+        protected List<ResolveInfo> mInstalledAppInfo;
+        protected List<AppItem> mInstalledApps = new LinkedList<AppItem>();
+
+        private void reloadList() {
+            final Handler handler = new Handler();
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    synchronized (mInstalledApps) {
+                        mInstalledApps.clear();
+                        for (ResolveInfo info : mInstalledAppInfo) {
+                            final AppItem item = new AppItem();
+                            item.title = info.loadLabel(mPackageManager);
+                            item.icon = info.loadIcon(mPackageManager);
+                            item.packageName = info.activityInfo.packageName;
+                            handler.post(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    int index = Collections.binarySearch(mInstalledApps, item);
+                                    if (index < 0) {
+                                        index = -index - 1;
+                                        mInstalledApps.add(index, item);
+                                    }
+                                    notifyDataSetChanged();
+                                }
+                            });
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        public AppAdapter(List<ResolveInfo> installedAppsInfo) {
+            mInstalledAppInfo = installedAppsInfo;
+        }
+
+        public void update() {
+            reloadList();
+        }
+
+        @Override
+        public int getCount() {
+            return mInstalledApps.size();
+        }
+
+        @Override
+        public AppItem getItem(int position) {
+            return mInstalledApps.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return mInstalledApps.get(position).packageName.hashCode();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView != null) {
+                holder = (ViewHolder) convertView.getTag();
+            } else {
+                final LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.preference_icon, null, false);
+                holder = new ViewHolder();
+                convertView.setTag(holder);
+                holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+                holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            }
+            AppItem applicationInfo = getItem(position);
+
+            if (holder.title != null) {
+                holder.title.setText(applicationInfo.title);
+            }
+            if (holder.icon != null) {
+                Drawable loadIcon = applicationInfo.icon;
+                holder.icon.setImageDrawable(loadIcon);
+            }
+            return convertView;
+        }
+    }
+
+    static class ViewHolder {
+        TextView title;
+        ImageView icon;
+    }
+}

src/com/android/settings/slim/LockscreenInterface.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2013 Slimroms
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +39,7 @@ import android.view.Window;
 import android.widget.Toast;
 
 import com.android.internal.view.RotationPolicy;
+import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -60,6 +62,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
     private static final String PREF_LOCKSCREEN_AUTO_ROTATE = "lockscreen_auto_rotate";
+    private static final String PREF_LOCKSCREEN_EIGHT_TARGETS = "lockscreen_eight_targets";
+    private static final String PREF_LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS = "lockscreen_shortcuts_longpress";
 
     private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
     private static final String KEY_BACKGROUND_ALPHA_PREF = "lockscreen_alpha";
@@ -70,6 +75,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
     private ListPreference mCustomBackground;
     private SeekBarPreference mBgAlpha;
     private CheckBoxPreference mLockscreenAutoRotate;
+    private CheckBoxPreference mLockscreenEightTargets;
+    private Preference mShortcuts;
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
 
     private int mUnsecureUnlockMethod;
     private boolean mIsScreenLarge;
@@ -131,10 +139,22 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         mBatteryStatus.setOnPreferenceChangeListener(this);
         setBatteryStatusSummary();
 
-        mLockscreenAutoRotate = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_AUTO_ROTATE);
+        mLockscreenAutoRotate = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_AUTO_ROTATE);
         int defaultValue = getResources().getBoolean(com.android.internal.R.bool.config_enableLockScreenRotation) ? 1 : 0;
         mLockscreenAutoRotate.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_AUTO_ROTATE, defaultValue) == 1);
+
+        mLockscreenEightTargets = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_EIGHT_TARGETS);
+        mLockscreenEightTargets.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_EIGHT_TARGETS, 0) == 1);
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 0) == 1);
+        mLockscreenShortcutsLongpress.setEnabled(!mLockscreenEightTargets.isChecked());
+
+        mShortcuts = (Preference) findPreference(PREF_LOCKSCREEN_SHORTCUTS);
+        mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
 
         if (RotationPolicy.isRotationLocked(getActivity())) {
             mLockscreenAutoRotate.setEnabled(false);
@@ -153,6 +173,14 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         if (mUnsecureUnlockMethod != 1) {
              PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
              getPreferenceScreen().removePreference(sliderCategory);
+        }
+
+        if (!Utils.isPhone(getActivity())) {
+             // Nothing for tablets and large screen devices
+             PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
+             sliderCategory.removePreference(mShortcuts);
+             sliderCategory.removePreference(mLockscreenShortcutsLongpress);
+             sliderCategory.removePreference(mLockscreenEightTargets);
         }
 
         setBatteryStatusSummary();
@@ -178,6 +206,23 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         if (preference == mLockscreenAutoRotate) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_AUTO_ROTATE, mLockscreenAutoRotate.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mLockscreenEightTargets) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_EIGHT_TARGETS, mLockscreenEightTargets.isChecked() ? 1 : 0);
+            mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
+            mLockscreenShortcutsLongpress.setEnabled(!mLockscreenEightTargets.isChecked());
+            Settings.System.putString(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_TARGETS, GlowPadView.EMPTY_TARGET);
+            for (File pic : mActivity.getFilesDir().listFiles()) {
+                if (pic.getName().startsWith("lockscreen_")) {
+                    pic.delete();
+                }
+            }
+            return true;
+        } else if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, mLockscreenShortcutsLongpress.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);

venky99/android_packages_apps_Settings-2

Conflicts:
	AndroidManifest.xml
	res/values/arrays.xml
	res/values/strings.xml
	res/xml/lockscreen_interface_settings.xml
	src/com/android/settings/liquid/LockscreenInterface.java

src/com/android/settings/liquid/ApplicationsDialogPreference.java
@@ -1,0 +1,166 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ * This code has been modified. Portions copyright (C) 2012, ParanoidAndroid Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ApplicationsDialogPreference extends SettingsPreferenceFragment {
+
+    public PackageManager mPackageManager;
+    public List<ResolveInfo> mInstalledApps;
+    public AppAdapter mAppAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public class AppItem implements Comparable<AppItem> {
+        public CharSequence title;
+        public String packageName;
+        public Drawable icon;
+
+        @Override
+        public int compareTo(AppItem another) {
+            return this.title.toString().compareTo(another.title.toString());
+        }
+    }
+
+    public class AppAdapter extends BaseAdapter {
+        protected List<ResolveInfo> mInstalledAppInfo;
+        protected List<AppItem> mInstalledApps = new LinkedList<AppItem>();
+
+        private void reloadList() {
+            final Handler handler = new Handler();
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    synchronized (mInstalledApps) {
+                        mInstalledApps.clear();
+                        for (ResolveInfo info : mInstalledAppInfo) {
+                            final AppItem item = new AppItem();
+                            item.title = info.loadLabel(mPackageManager);
+                            item.icon = info.loadIcon(mPackageManager);
+                            item.packageName = info.activityInfo.packageName;
+                            handler.post(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    int index = Collections.binarySearch(mInstalledApps, item);
+                                    if (index < 0) {
+                                        index = -index - 1;
+                                        mInstalledApps.add(index, item);
+                                    }
+                                    notifyDataSetChanged();
+                                }
+                            });
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        public AppAdapter(List<ResolveInfo> installedAppsInfo) {
+            mInstalledAppInfo = installedAppsInfo;
+        }
+
+        public void update() {
+            reloadList();
+        }
+
+        @Override
+        public int getCount() {
+            return mInstalledApps.size();
+        }
+
+        @Override
+        public AppItem getItem(int position) {
+            return mInstalledApps.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return mInstalledApps.get(position).packageName.hashCode();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView != null) {
+                holder = (ViewHolder) convertView.getTag();
+            } else {
+                final LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.preference_icon, null, false);
+                holder = new ViewHolder();
+                convertView.setTag(holder);
+                holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+                holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            }
+            AppItem applicationInfo = getItem(position);
+
+            if (holder.title != null) {
+                holder.title.setText(applicationInfo.title);
+            }
+            if (holder.icon != null) {
+                Drawable loadIcon = applicationInfo.icon;
+                holder.icon.setImageDrawable(loadIcon);
+            }
+            return convertView;
+        }
+    }
+
+    static class ViewHolder {
+        TextView title;
+        ImageView icon;
+    }
+}

src/com/android/settings/liquid/LockscreenInterface.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2013 Slimroms
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +39,7 @@ import android.view.Window;
 import android.widget.Toast;
 
 import com.android.internal.view.RotationPolicy;
+import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -60,6 +62,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
     private static final String KEY_LOCKSCREEN_AUTO_ROTATE = "lockscreen_auto_rotate";
+    private static final String KEY_LOCKSCREEN_EIGHT_TARGETS = "lockscreen_eight_targets";
+    private static final String KEY_LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+    private static final String KEY_LOCKSCREEN_SHORTCUTS_LONGPRESS = "lockscreen_shortcuts_longpress";
     private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
     private static final String KEY_BACKGROUND_ALPHA_PREF = "lockscreen_alpha";
     private static final String KEY_LOCKSCREEN_TEXT_COLOR = "lockscreen_text_color";
@@ -71,7 +76,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
     private SeekBarPreference mBgAlpha;
     private CheckBoxPreference mLockscreenAutoRotate;
     private ColorPickerPreference mLockscreenTextColor;
-
+    private CheckBoxPreference mLockscreenEightTargets;
+    private Preference mShortcuts;
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
     private int mUnsecureUnlockMethod;
     private boolean mIsScreenLarge;
 
@@ -106,23 +113,22 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         prefs = getPreferenceScreen();
 
         mAdditionalOptions = (PreferenceCategory) prefs.findPreference(KEY_ADDITIONAL_OPTIONS);
-
         mCustomBackground = (ListPreference) findPreference(KEY_BACKGROUND_PREF);
         mCustomBackground.setOnPreferenceChangeListener(this);
         wallpaperImage = new File(mActivity.getFilesDir()+"/lockwallpaper");
         wallpaperTemporary = new File(mActivity.getCacheDir()+"/lockwallpaper.tmp");
 
         float bgAlpha;
-        try{
+        try {
             bgAlpha = Settings.System.getFloat(getActivity()
                 .getContentResolver(),
                 Settings.System.LOCKSCREEN_ALPHA);
-         }catch (Exception e) {
+        } catch (Exception e) {
             bgAlpha = 0;
             bgAlpha = Settings.System.getFloat(getActivity()
                 .getContentResolver(),
                 Settings.System.LOCKSCREEN_ALPHA, 0.0f);
-         }
+        }
         mBgAlpha = (SeekBarPreference) findPreference(KEY_BACKGROUND_ALPHA_PREF);
         mBgAlpha.setInitValue((int) (bgAlpha * 100));
         mBgAlpha.setProperty(Settings.System.LOCKSCREEN_ALPHA);
@@ -133,9 +139,22 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         setBatteryStatusSummary();
 
         mLockscreenAutoRotate = (CheckBoxPreference)findPreference(KEY_LOCKSCREEN_AUTO_ROTATE);
+
         int defaultValue = getResources().getBoolean(com.android.internal.R.bool.config_enableLockScreenRotation) ? 1 : 0;
         mLockscreenAutoRotate.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_AUTO_ROTATE, defaultValue) == 1);
+
+        mLockscreenEightTargets = (CheckBoxPreference) findPreference(KEY_LOCKSCREEN_EIGHT_TARGETS);
+        mLockscreenEightTargets.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_EIGHT_TARGETS, 0) == 1);
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(KEY_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 0) == 1);
+        mLockscreenShortcutsLongpress.setEnabled(!mLockscreenEightTargets.isChecked());
+
+        mShortcuts = (Preference) findPreference(KEY_LOCKSCREEN_SHORTCUTS);
+        mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
 
         if (RotationPolicy.isRotationLocked(getActivity())) {
             mLockscreenAutoRotate.setEnabled(false);
@@ -158,6 +177,14 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
 
         mLockscreenTextColor = (ColorPickerPreference) findPreference(KEY_LOCKSCREEN_TEXT_COLOR);
         mLockscreenTextColor.setOnPreferenceChangeListener(this);
+
+        if (!Utils.isPhone(getActivity())) {
+             // Nothing for tablets and large screen devices
+             PreferenceCategory sliderCategory = (PreferenceCategory) findPreference(KEY_SLIDER_OPTIONS);
+             sliderCategory.removePreference(mShortcuts);
+             sliderCategory.removePreference(mLockscreenShortcutsLongpress);
+             sliderCategory.removePreference(mLockscreenEightTargets);
+        }
 
         setBatteryStatusSummary();
         updateCustomBackgroundSummary();
@@ -182,6 +209,23 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements P
         if (preference == mLockscreenAutoRotate) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_AUTO_ROTATE, mLockscreenAutoRotate.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mLockscreenEightTargets) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_EIGHT_TARGETS, mLockscreenEightTargets.isChecked() ? 1 : 0);
+            mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
+            mLockscreenShortcutsLongpress.setEnabled(!mLockscreenEightTargets.isChecked());
+            Settings.System.putString(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_TARGETS, GlowPadView.EMPTY_TARGET);
+            for (File pic : mActivity.getFilesDir().listFiles()) {
+                if (pic.getName().startsWith("lockscreen_")) {
+                    pic.delete();
+                }
+            }
+            return true;
+        } else if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, mLockscreenShortcutsLongpress.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);