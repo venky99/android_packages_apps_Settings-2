5cb4bdf2c1a2f447fd92de89e1cf4a6c40632688
dhacker29 <davidhackerdvm@gmail.com>
2013-02-14 00:08:47 -0600

    Fix Display Settings fc

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -44,6 +44,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.view.RotationPolicy;
+import com.android.settings.DreamSettings;
 import com.android.settings.liquid.DisplayRotation;
 
 import java.util.ArrayList;
