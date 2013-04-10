471d043cfb6841a61256b458fdc13e5b4c335ebf
liquid0624 <rjbirnesser@gmail.com>
2013-04-09 20:32:59 -0400

    Cleanup Developer Settings Whitespace

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Portions Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -207,7 +208,6 @@ public class DevelopmentSettings extends PreferenceFragment
     private boolean mDialogClicked;
     private Dialog mEnableDialog;
     private Dialog mAdbDialog;
-
     private Dialog mAdbTcpDialog;
     private Dialog mRootDialog;
 
@@ -1314,7 +1314,6 @@ public class DevelopmentSettings extends PreferenceFragment
                 mLastEnabledState = true;
                 setPrefsEnabledState(mLastEnabledState);
             }
-
         } else if (dialog == mRootDialog) {
             if (which == DialogInterface.BUTTON_POSITIVE) {
                 writeRootAccessOptions(mSelectedRootValue);
