b5d37c88b2ed649b4d0e159682e8e9e14f84cb96
liquid0624 <rjbirnesser@gmail.com>
2013-04-15 20:06:24 -0400

    Cleanup Interface Settings/Fix Force Dual Panel

--- src/com/android/settings/liquid/InterfaceSettings.java
+++ src/com/android/settings/liquid/InterfaceSettings.java
@@ -48,11 +48,11 @@ import android.view.MenuItem;
 import android.view.IWindowManager;
 import android.view.Window;
 import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.EditText;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import android.widget.EditText;
 
 public class InterfaceSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -88,15 +88,6 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
         addPreferencesFromResource(R.xml.interface_settings);
 
         PreferenceScreen prefs = getPreferenceScreen();
-        mLcdDensity = findPreference("lcd_density_setup");
-        mLcdDensity.setOnPreferenceChangeListener(this);
-        String currentProperty = SystemProperties.get("ro.sf.lcd_density");
-        try {
-            newDensityValue = Integer.parseInt(currentProperty);
-        } catch (Exception e) {
-            getPreferenceScreen().removePreference(mLcdDensity);
-        }
-        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + currentProperty);
 
         mAdvanced = (PreferenceCategory) prefs.findPreference(ADVANCED_SETTINGS);
 
@@ -111,6 +102,42 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
 
         updateCustomLabelTextSummary();
 
+        mLcdDensity = findPreference("lcd_density_setup");
+        mLcdDensity.setOnPreferenceChangeListener(this);
+        String currentProperty = SystemProperties.get("ro.sf.lcd_density");
+        try {
+            newDensityValue = Integer.parseInt(currentProperty);
+        } catch (Exception e) {
+            getPreferenceScreen().removePreference(mLcdDensity);
+        }
+        mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + currentProperty);
+
+        mRamBar = findPreference(KEY_RECENTS_RAM_BAR);
+        mRamBar.setOnPreferenceChangeListener(this);
+        updateRamBar();
+
+        mDualPane = (CheckBoxPreference) findPreference(KEY_FORCE_DUAL_PANE);
+        mDualPane.setOnPreferenceChangeListener(this);
+        boolean preferDualPane = getResources().getBoolean(
+                com.android.internal.R.bool.preferences_prefer_dual_pane);
+        boolean dualPaneMode = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.DUAL_PANE_PREFS, (preferDualPane ? 1 : 0)) == 1;
+        mDualPane.setChecked(dualPaneMode);
+
+        mLowBatteryWarning = (ListPreference) findPreference(KEY_LOW_BATTERY_WARNING_POLICY);
+        mLowBatteryWarning.setOnPreferenceChangeListener(this);
+        int lowBatteryWarning = Settings.System.getInt(getActivity().getContentResolver(),
+                                    Settings.System.POWER_UI_LOW_BATTERY_WARNING_POLICY, 0);
+        mLowBatteryWarning.setValue(String.valueOf(lowBatteryWarning));
+        mLowBatteryWarning.setSummary(mLowBatteryWarning.getEntry());
+
+        mVibrationMultiplier = (ListPreference) findPreference(KEY_VIBRATION_MULTIPLIER);
+        mVibrationMultiplier.setOnPreferenceChangeListener(this);
+        String currentValue = Float.toString(Settings.System.getFloat(getActivity()
+                .getContentResolver(), Settings.System.VIBRATION_MULTIPLIER, 1));
+        mVibrationMultiplier.setValue(currentValue);
+        mVibrationMultiplier.setSummary(currentValue);
+
         // Only show the hardware keys config on a device that does not have a navbar
         IWindowManager windowManager = IWindowManager.Stub.asInterface(
                 ServiceManager.getService(Context.WINDOW_SERVICE));
@@ -124,27 +151,6 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
         }
 
         mNotifStyle = findPreference(KEY_NOTIF_STYLE);
-        mRamBar = findPreference(KEY_RECENTS_RAM_BAR);
-        mRamBar.setOnPreferenceChangeListener(this);
-        updateRamBar();
-
-        mDualPane = (CheckBoxPreference) findPreference(KEY_FORCE_DUAL_PANE);
-        mDualPane.setOnPreferenceChangeListener(this);
-        boolean preferDualPane = getResources().getBoolean(
-                com.android.internal.R.bool.preferences_prefer_dual_pane);
-        boolean dualPaneMode = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.DUAL_PANE_PREFS, (preferDualPane ? 1 : 0)) == 1;
-        mDualPane.setChecked(dualPaneMode);
-    }
-
-    private void updateCustomLabelTextSummary() {
-        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
-                Settings.System.CUSTOM_CARRIER_LABEL);
-        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
-            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
-        } else {
-            mCustomLabel.setSummary(mCustomLabelText);
-        }
     }
 
     private void updateRamBar() {
@@ -155,22 +161,16 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
         } else {
             mRamBar.setSummary(getResources().getString(R.string.ram_bar_color_disabled));
         }
+    }
 
-        mVibrationMultiplier = (ListPreference) findPreference(KEY_VIBRATION_MULTIPLIER);
-        if(mVibrationMultiplier != null) {
-            mVibrationMultiplier.setOnPreferenceChangeListener(this);
-            String currentValue = Float.toString(Settings.System.getFloat(getActivity()
-                    .getContentResolver(), Settings.System.VIBRATION_MULTIPLIER, 1));
-            mVibrationMultiplier.setValue(currentValue);
-            mVibrationMultiplier.setSummary(currentValue);
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
         }
-
-        mLowBatteryWarning = (ListPreference) findPreference(KEY_LOW_BATTERY_WARNING_POLICY);
-        mLowBatteryWarning.setOnPreferenceChangeListener(this);
-        int lowBatteryWarning = Settings.System.getInt(getActivity().getContentResolver(),
-                                    Settings.System.POWER_UI_LOW_BATTERY_WARNING_POLICY, 0);
-        mLowBatteryWarning.setValue(String.valueOf(lowBatteryWarning));
-        mLowBatteryWarning.setSummary(mLowBatteryWarning.getEntry());
     }
 
     @Override
@@ -210,7 +210,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
         } else if (preference == mDualPane) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.DUAL_PANE_PREFS,
-                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+                    (Boolean) newValue ? 1 : 0);
             return true;
         }
         return false;
@@ -245,11 +245,9 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements OnP
                     // Canceled.
                 }
             });
-
             alert.show();
             return true;
         }
     };
-
 }
 
